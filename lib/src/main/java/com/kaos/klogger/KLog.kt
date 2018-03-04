0fb103fee4604e6fa334352a91745696b7efbf9e
kaosdev <elialotti.dev@gmail.com>
2018-03-04 11:39:13 +0100

    Adjusted inlining, added a base method, added TAG extension

--- lib/src/main/java/com/kaos/klogger/KLog.kt
+++ lib/src/main/java/com/kaos/klogger/KLog.kt
@@ -1,6 +1,10 @@
 package com.kaos.klogger
 
 import android.util.Log
+import com.kaos.klogger.Methods.*
+import java.util.*
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
 
 /**
  * Created by elial on 2/26/2018.
@@ -8,105 +12,76 @@ import android.util.Log
  * Description:
  *
  */
-inline infix fun <reified T> T.tryLog(f: () -> Unit) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
+enum class Methods { Info, Error, Debug, Verbose, Warn, Wtf }
+
+/** Standard Tag **/
+val Any?.TAG: String
+    get() = when(this) {
+        is String, Int -> toString()
+        is Any -> this::class.java.simpleName
+        else -> "null"
     }
 
-    try { tag i(f) } catch(e: Exception) {
-        tag e e.message!!
-    }
+/** Base Method **/
+fun Any?.log(method: Methods, message: Any? = null) = when(method) {
+    Info -> Log.i(TAG, message.toString())
+    Error -> Log.e(TAG, message.toString())
+    Debug -> Log.d(TAG, message.toString())
+    Verbose -> Log.v(TAG, message.toString())
+    Warn -> Log.w(TAG, message.toString())
+    Wtf -> Log.wtf(TAG, message.toString())
+}
+
+/** Try catch **/
+inline infix fun <T> T.tryLog(f: T.() -> Any?) =
+        try { this i f } catch(e: Exception) {
+            this e e.message
+        }
+
+/** Log result and time **/
+infix fun <T> T.logTime(f: T.() -> Any?) {
+    i { "result: ${f()}, time: ${measureNanoTime {f()}}ns" }
 }
 
 /** Info **/
 //String
-inline infix fun <reified T> T.i(mess: String) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.i(tag,mess)
-}
+infix fun Any?.i(mess: Any?) = log(Info,mess)
+
 //Lambda
-inline infix fun <reified T> T.i(call: () -> Any?) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.i(tag,"${call()}")
-}
+inline infix fun <T> T.i(call: T.() -> Any?): Any? = log(Info,call())
 
 /** Debug **/
 //String
-inline infix fun <reified T> T.d(mess: String) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.d(tag,mess)
-}
+infix fun <T> T.d(mess: Any?) = log(Debug,mess)
+
 //Lambda
-inline infix fun <reified T> T.d(call: () -> Any?) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.d(tag,"${call()}")
-}
+inline infix fun <T> T.d(call: T.() -> Any?) = log(Debug,call())
 
 /** Error **/
 //String
-inline infix fun <reified T> T.e(mess: String) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.e(tag,mess)
-}
+infix fun <T> T.e(mess: Any?) = log(Error,mess)
+
 //Lambda
-inline infix fun <reified T> T.e(call: () -> Any?)  {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
+inline infix fun <T> T.e(call: T.() -> Any?) = log(Error,call())
 
-    Log.e(tag,"${call()}")
-}
-
-/** VERBOSE **/
+/** Verbose **/
 //String
-inline infix fun <reified T> T.v(mess: String) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.v(tag,mess)
-}
-//Lambda
-inline infix fun <reified T> T.v(call: () -> Any?)  {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.v(tag,"${call()}")
-}
+infix fun <T> T.v(mess: Any?) = log(Verbose,mess)
 
-/** WARN **/
-//String
-inline infix fun <reified T> T.w(mess: String) {
-    val tag = when(this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.w(tag,mess)
-}
 //Lambda
-inline infix fun <reified T> T.w(call: () -> Any?) {
-    val tag = when (this) {
-        is String -> this
-        else -> T::class.java.simpleName
-    }
-    Log.w(tag, "${call()}")
-}
+inline infix fun <T> T.v(call: T.() -> Any?) = log(Verbose,call())
+
+/** Warn **/
+//String
+infix fun <T> T.w(mess: Any?) = log(Warn,mess)
+
+//Lambda
+inline infix fun <T> T.w(call: T.() -> Any?) = log(Warn,call())
+
+/** Wtf **/
+//String
+infix fun <T> T.wtf(mess: Any?) = log(Wtf,mess)
+
+//Lambda
+inline infix fun <T> T.wtf(call: T.() -> Any?) = log(Wtf,call())
 
